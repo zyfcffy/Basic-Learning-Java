2d90099a6d4fa405498ba5d603d607451b171879
fufangyuan <zyfcffy@163.com>
2021-04-06 21:53:56 +0800

    [fufangyuan] first commit

---
+++ src/main/java/Basic/Thread/RunnableImpl.java
@@ -0,0 +1,13 @@
+package Basic.Thread;
+
+//1、创建一个Runnable接口实现类
+public class RunnableImpl implements Runnable {
+    //2、在实现类中重写Runnable接口的run方法，设置线程任务
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+"-->"+i);
+        }
+    }
+}
+
