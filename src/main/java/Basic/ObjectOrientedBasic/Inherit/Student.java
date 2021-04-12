4f87d3e25751f4f861786f998c6fb760911c9b04
fufangyuan <zyfcffy@163.com>
2021-04-12 23:08:34 +0800

    [fangyuan] 2021/04/10 refactor package, add oop

---
+++ src/main/java/Basic/ObjectOrientedBasic/Inherit/Student.java
@@ -0,0 +1,18 @@
+package Basic.ObjectOrientedBasic.Inherit;
+
+public class Student extends Person {
+    private int score;
+
+    public Student(String name, int age, int score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
--- src/main/java/Basic/RDD/Map.java
+++ src/main/java/Basic/RDD/Map.java
@@ -1,5 +1,7 @@
 package Basic.RDD;
-
+/*
+* Map类, .map方法, .flatmap方法
+* */
 public class Map {
 
 }
