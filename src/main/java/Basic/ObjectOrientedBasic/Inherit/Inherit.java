c9aff736022b55262e991383fbaeafc3c868c962
fangyuan.fu <zyfcffy@163.com>
2021-04-13 13:08:07 +0800

    [fangyuan] add instanceof func in inherit

--- src/main/java/Basic/ObjectOrientedBasic/Inherit/Inherit.java
+++ src/main/java/Basic/ObjectOrientedBasic/Inherit/Inherit.java
@@ -19,6 +19,20 @@ public class Inherit {
         Student s = (Student) xiaozhang;
         System.out.println(s.getName());
         System.out.println(s.getScore());
+
+        //可以使用 instanceof 来判断是不是某种类型的实例
+        System.out.println(xiaoxu instanceof Student);  //false
+        //向下转型前可以判断变量是否指向的实例是指定的类型，或者是这个类型的子类，再进行转型
+        Person p1 = new Student("s", 18, 90);
+        if (p1 instanceof Student){
+            Student student = (Student) p1;
+        }
+        //Java 14 之后，判断instance of之后，可以直接转型为指定变量
+//        Object obj = "hello";
+//        if (obj instanceof String ss) {
+//            // 直接使用变量 s
+//            System.out.println(ss.toUpperCase());
+//        }
     }
 
 }
