727f35ed3962fe30f693e05aed95f1699ca27f57
fufangyuan <zyfcffy@163.com>
2021-04-19 20:57:47 +0800

    [fangyuan] 2021/04/19 add reflection ClassMethod

---
+++ src/main/java/Basic/ObjectOrientedBasic/Reflection/ClassFieldAndMethod.java
@@ -0,0 +1,91 @@
+package Basic.ObjectOrientedBasic.Reflection;
+
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/*
+* 访问字段
+* Field getField(name)：根据字段名获取某个public的field（包括父类）
+* Field getDeclaredField(name)：根据字段名获取当前类的某个field（不包括父类）
+* 访问方法
+* Method getMethod(name, Class...)：获取某个public的Method（包括父类）
+* Method getDeclaredMethod(name, Class...)：获取当前类的某个Method（不包括父类）
+* */
+public class ClassFieldAndMethod {
+    public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<Student> sc = Student.class;
+        // name 和 score 是 private ，本类的 score 用 getDeclaredField 会将所有的字段都返回
+        // 父类的字段是private的,子类无法获取父类私有实例
+        // System.out.println(sc.getDeclaredField("name")); //Field 'name' is not declared in class 'Basic.ObjectOrientedBasic.Reflection.Student'
+        // 要通过反射拿到父类的私有属性和方法，要使用getSuperClass()
+        System.out.println(sc.getSuperclass().getDeclaredField("name"));
+        System.out.println(sc.getDeclaredField("score"));
+        System.out.println(sc.getField("clazz"));
+
+        System.out.println(sc.getMethod("getClazz", String.class));
+        System.out.println(sc.getDeclaredMethod("getScore"));
+
+        // 用反射来调用方法，用invoke方法
+        /*
+        * String s = "hello tw";
+        * String r = s.subString(6);  // tw
+        * task：
+        * 1、获取方法实例 getMethod()
+        * 2、实例调用方法 Method 的 invoke 方法
+        */
+        String s = "hello tw";
+        Method m  = String.class.getMethod("substring", int.class);
+        String result = (String) m.invoke(s, 6);
+        System.out.println(result);
+
+        // 调用非Public方法, setAccessible(true)，没有的话，会报 java.lang.NoSuchMethodException
+        Student student = new Student();
+        Method method1 = student.getClass().getDeclaredMethod("setScore", int.class);
+        Method method2 = student.getClass().getDeclaredMethod("getScore");
+        method1.setAccessible(true);
+        method2.setAccessible(true);
+        method1.invoke(student, 90);
+        //System.out.println(student.score);
+        Object o = method2.invoke(student);
+        System.out.println(o.toString());
+
+        // 多态
+        /*
+        * Person p = new Student();
+        * p.hello();  // 调用的是子类覆写的方法
+        * */
+        Method method = student.getClass().getMethod("hello");
+        method.invoke(student);  //Student say hello
+        Method method3 = Person.class.getMethod("hello");
+        method3.invoke(new Student()); //Student say hello
+    }
+}
+class Person {
+    private String name;
+    public void hello(){
+        System.out.println("Person say hello");
+    }
+}
+
+class Student extends Person {
+    int score;
+    public int clazz;
+
+    private void setScore(int score){
+        this.score = score;
+    }
+
+    private int getScore(){
+        return this.score;
+    }
+
+    public int getClazz(String name){
+        return 2;
+    }
+
+    @Override
+    public void hello() {
+        System.out.println("Student say hello");
+    }
+}
