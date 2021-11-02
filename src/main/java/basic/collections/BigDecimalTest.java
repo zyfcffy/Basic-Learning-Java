c51b3fbe02e551d76572a7c48acb2cd4bcd2ff0a
Fangyuan Fu <fangyuan.fu@thoughtworks.com>
2021-11-02 16:10:19 +0800

    [fangyuan] Add Bigdecimal test string

---
+++ src/main/java/basic/collections/BigDecimalTest.java
@@ -0,0 +1,10 @@
+package basic.collections;
+
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+  public static void main(String[] args) {
+    System.out.println(new BigDecimal("abc"));
+  }
+
+}
